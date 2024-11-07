16d702b2a52c3c09ea372c5d56501c358ae4780c
gilzoide <gilzoide@gmail.com>
2024-11-07 17:27:25 -0300

    Add support for setting header/implementation paths separately in CMake

--- file2c.cmake
+++ file2c.cmake
@@ -10,6 +10,8 @@ Usage:
 add_file2c(
   <generated-target-name>
   INPUT <input-file>  # Input file name, required
+  [IMPL_OUTPUT <impl-output>]  # Implementation file output path.
+  [HEADER_OUTPUT <header-output>]  # Header file output path.
   [SYMBOL <symbol>]  # Symbol used for the generated global variable.
                      # Defaults to the name of the <input-file> without extensions
   [TEXT]  # If passed, the variable is defined as "const char *" and is guaranteed to end in a null character.
@@ -19,7 +21,7 @@ target_link_libraries(<target-that-uses-file-contents> <generated-target-name>)
 ]]
 function(add_file2c arg_file2c_TARGET)
   set(options TEXT)
-  set(oneValueArgs INPUT SYMBOL)
+  set(oneValueArgs INPUT IMPL_OUTPUT HEADER_OUTPUT SYMBOL)
   set(multiValueArgs)
   cmake_parse_arguments(arg_file2c "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
@@ -31,6 +33,12 @@ function(add_file2c arg_file2c_TARGET)
   endif()
   if(NOT DEFINED arg_file2c_SYMBOL)
     get_filename_component(arg_file2c_SYMBOL "${arg_file2c_INPUT}" NAME_WE)
+  endif()
+  if(NOT DEFINED arg_file2c_IMPL_OUTPUT)
+    set(arg_file2c_IMPL_OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/${arg_file2c_TARGET}/${arg_file2c_SYMBOL}.c")
+  endif()
+  if(NOT DEFINED arg_file2c_HEADER_OUTPUT)
+    set(arg_file2c_HEADER_OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/${arg_file2c_TARGET}/${arg_file2c_SYMBOL}.h")
   endif()
 
   get_filename_component(arg_file2c_INPUT_ABSOLUTE "${arg_file2c_INPUT}" ABSOLUTE)
@@ -44,32 +52,30 @@ function(add_file2c arg_file2c_TARGET)
   endif()
 
   find_package(Python3 REQUIRED COMPONENTS Interpreter)
-  set(arg_file2c_OUTPUT_DIR "${CMAKE_CURRENT_BINARY_DIR}/${arg_file2c_TARGET}")
-  set(arg_file2c_C_OUTPUT "${arg_file2c_OUTPUT_DIR}/${arg_file2c_SYMBOL}.c")
-  set(arg_file2c_H_OUTPUT "${arg_file2c_OUTPUT_DIR}/${arg_file2c_SYMBOL}.h")
   add_custom_command(
     OUTPUT
-      "${arg_file2c_C_OUTPUT}"
+      "${arg_file2c_IMPL_OUTPUT}"
     COMMAND
-      ${Python3_EXECUTABLE} "${_file2c_py}" ${file2c_py_args} -o "${arg_file2c_C_OUTPUT}"
+      ${Python3_EXECUTABLE} "${_file2c_py}" ${file2c_py_args} -o "${arg_file2c_IMPL_OUTPUT}"
     DEPENDS
       "${_file2c_py}"
       "${arg_file2c_INPUT}"
   )
   add_custom_command(
     OUTPUT
-      "${arg_file2c_H_OUTPUT}"
+      "${arg_file2c_HEADER_OUTPUT}"
     COMMAND
-      ${Python3_EXECUTABLE} "${_file2c_py}" ${file2c_py_args} -o "${arg_file2c_H_OUTPUT}" --header
+      ${Python3_EXECUTABLE} "${_file2c_py}" ${file2c_py_args} -o "${arg_file2c_HEADER_OUTPUT}" --header
     DEPENDS
       "${_file2c_py}"
   )
   add_library(${arg_file2c_TARGET} OBJECT
-    "${arg_file2c_H_OUTPUT}"
-    "${arg_file2c_C_OUTPUT}"
+    "${arg_file2c_IMPL_OUTPUT}"
+    "${arg_file2c_HEADER_OUTPUT}"
   )
+  get_filename_component(arg_file2c_HEADER_DIR "${arg_file2c_HEADER_OUTPUT}" DIRECTORY)
   target_include_directories(${arg_file2c_TARGET}
-    PUBLIC "${arg_file2c_OUTPUT_DIR}"
+    PUBLIC "${arg_file2c_HEADER_DIR}"
   )
 endfunction()
 
